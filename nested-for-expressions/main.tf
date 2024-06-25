780f282daf11ef299a38841a8e635d372db4cd62
thenickstrick <57534232+thenickstrick@users.noreply.github.com>
2024-06-25 09:18:59 -0500

    add notes
    add some explanations to break down what's happening in the for-expression

--- nested-for-expressions/main.tf
+++ nested-for-expressions/main.tf
@@ -7,7 +7,8 @@ locals {
 
   # create for-expression to iterate over sample data to extract more desirable/specific output
   expected_output = [
-    # adding cities here is necessary for terrafrom to properly output the contninent map from the root map of values
+    # adding cities here is necessary for terrafrom to properly output the contninent values from the root map 
+    # for this key (continent) in this value (city) in this map (root_node), output this object (continent)
     for continent, cities in local.root_node : {
       display_name = "${continent}"
     }
