2101081ef1107af5bd0834b3820ff60bb09e8d08
thenickstrick <57534232+thenickstrick@users.noreply.github.com>
2024-06-25 00:15:39 -0500

    zooming in on the sample data

--- nested-for-loops/main.tf
+++ nested-for-loops/main.tf
@@ -1,7 +1,15 @@
 locals {
+  # map the raw content of the entire sample data file
   raw_content = jsondecode(file("${path.module}/sample.json"))
+  # create ref to key to test extracting specific data out of the sample file
+  root_node = local.raw_content["zscalerthree.net"]
 }
 
-output "raw_content" {
-  value = local.raw_content
+# output "raw_content" {
+#   value = local.raw_content
+# }
+
+# output the data that was extracted
+output "root_node" {
+  value = local.root_node
 }
