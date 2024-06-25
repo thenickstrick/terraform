2d8b489edeab0993c6f909d75100133253aef88c
thenickstrick <57534232+thenickstrick@users.noreply.github.com>
2024-06-25 00:26:29 -0500

    add continent for-expression
    add for-expression to extract just the continents from the sample data

--- nested-for-expressions/main.tf
+++ nested-for-expressions/main.tf
@@ -1,8 +1,16 @@
 locals {
   # map the raw content of the entire sample data file
   raw_content = jsondecode(file("${path.module}/sample.json"))
+
   # create ref to key to test extracting specific data out of the sample file
   root_node = local.raw_content["zscalerthree.net"]
+
+  # create for-expression to iterate over sample data to extract more desirable/specific output
+  expected_output = [
+    for continent, cities in local.root_node : {
+      display_name = "$(continent)"
+    }
+  ]
 }
 
 # output "raw_content" {
@@ -10,6 +18,10 @@ locals {
 # }
 
 # output the data that was extracted
-output "root_node" {
-  value = local.root_node
+# output "root_node" {
+#   value = local.root_node
+# }
+
+output "expected_output" {
+  value = local.expected_output
 }
